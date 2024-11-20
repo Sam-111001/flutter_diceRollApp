1c90908c18e74cbdce9f4a8908ba9ae8d65668cb
Shamant Myageri <code.shamant@gmail.com>
2024-11-20 21:05:49 +0530

    Created a userdefined widget called gradient container

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,36 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 31, 1, 85),
+            Color.fromARGB(255, 48, 3, 132)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+          ),
+        ),
+      ),
+    );
+  }
 }
